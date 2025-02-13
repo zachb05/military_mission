661921a4bfcb56b2777aa23895a0165b7d67731b
Arnav Gupta <arnavg1cbe@gmail.com>
2025-02-12 20:15:31 -0500

    adjusted wheel mounting

--- motorDriver/motorDriver.ino
+++ motorDriver/motorDriver.ino
@@ -1,37 +1,42 @@
+void drive(int direction, int speed);
+
 void setup() {
   // put your setup code here, to run once:
-#define bin1 4
-#define bin2 6
-#define pwn 11
+  #define bin1 22
+  #define bin2 19
+  #define pwn 4
 
 
-pinMode(bin1, OUTPUT);
-pinMode(bin2, OUTPUT);
-pinMode(pwn, OUTPUT);
+  pinMode(bin1, OUTPUT);
+  pinMode(bin2, OUTPUT);
+  pinMode(pwn, OUTPUT);
 
 }
-
-void drive(int direction, int speed);
 
 void loop() {
   // put your main code here, to run repeatedly:
+  /*    analogWrite(pwn, 255);
+    digitalWrite(bin1, HIGH);
+    digitalWrite(bin2, LOW);
+  */
 
-for(int i = 0; i<100; i+=3)
-{
-  forwards(i, .05);
+  for(int i = 0; i<100; i+=3)
+  {
+    drive(1, i);
+  }
+
+  for(int i = 0; i<100; i+=3)
+  {
+    drive(-1, i);
+  }
+
+  digitalWrite(bin1, LOW);
+  delay(1000);
+
 }
 
 
-
-digitalWrite(bin1, LOW);
-delay(1000);
-
-
-}
-
-
-void drive(int direction, int speed)
-{
+void drive(int direction, int speed) {
   float new_speed = map(speed, 0, 100, 50, 255);
  
   if(direction == 1)//forwards
@@ -46,7 +51,7 @@ void drive(int direction, int speed)
     digitalWrite(bin1, LOW);
     digitalWrite(bin2, HIGH);
   }
-  else
+  else  
   {
     analogWrite(pwn, new_speed);
     digitalWrite(bin1, LOW);
